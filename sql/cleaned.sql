7f954e8c6b5a5ecd49f9038cc7ec41c1919f8b90
Michael Galo <michael@michaelthinks.co>
2025-08-20 13:40:29 -0500

    updated sql scripts to parse dates

--- sql/cleaned.sql
+++ sql/cleaned.sql
@@ -3,21 +3,22 @@ CREATE TABLE IF NOT EXISTS GOLD.nyc_salary_matches AS
 SELECT
     business_title AS posted_job_title,
     title_description AS matched_actual_payroll_title,
-    match_ratio AS match_score,
+    score AS match_score,
     salary_range_from AS posting_min_salary,
     salary_range_to AS posting_max_salary,
     base_salary AS actual_base_salary,
-    posting_duration AS posting_duration_days, -- FIXME: doesn't include
+    CAST(CAST(strptime(post_until, '%d-%b-%Y') AS DATE) - CAST(posting_date AS DATE) AS INTEGER) AS posting_duration_days,
     regular_gross_paid AS actual_gross_paid,
     total_ot_paid AS actual_ot_paid,
     total_other_pay AS actual_other_pay
-FROM
-    BRONZE.nyc_jobs_audited_fuzzy_raw;
+FROM BRONZE.jobs_to_lightcast_title_fuzzy_matches
+ORDER BY match_score DESC;
 
 CREATE TABLE IF NOT EXISTS GOLD.nyc_matched_job_posting_duration_SOC AS
-SELECT 
+SELECT
     business_title AS title,
     lightcast_matched_occupation,
     "Total Postings (Jan 2024 - Jun 2025)" AS total_postings,
     "Median Posting Duration" AS median_posting_duration
-FROM BRONZE.jobs_to_lightcast_title_fuzzy_matches;
+FROM BRONZE.jobs_to_lightcast_title_fuzzy_matches
+ORDER BY median_posting_duration DESC;
